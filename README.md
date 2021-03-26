6418ca2ed647880b8b0a0422eb4a998ad1f8f968
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-03-26 07:38:19 +0700

    add route post data siswa and multer

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 1. Membuat route
 
-2. Menghubungkan dengan Express JS
+2. Menghubungkan Node JS dengan Express JS
 
 3. CRUD API MongoDB
 
 4. Postman
+
+5. ObjectId (MongoDb)
+
+6. multer() , agar bisa digunakan pada form-data, default x-www-form-urlencoded : yarn add multer
--- routers.js
+++ routers.js
@@ -1,19 +1,21 @@
 const client = require('./connection')
 const express = require('express');
 const routers = express.Router();
-const ObjectId = require('mongodb').ObjectId
+const ObjectId = require('mongodb').ObjectId;
+const multer = require('multer');
 
+// cek koneksi
 // if (client.isConnected()){
 //     const db = client.db("sekolah")
 // } else{
 //     console.log('koneksi database gagal')
 // }
 
-//list siswa
+//daftar siswa siswa
 routers.get('/siswa', async (req, res) => {
     if (client.isConnected()) {
         const db = client.db('sekolah')
-        // kode untuk menampilkan list products res.send('menampilkan list products')
+        // kode untuk menampilkan list siswa res.send('menampilkan list siswa')
         const siswa = await db.collection('siswa').find().toArray()
         res.send({
             status: 'success',
@@ -32,14 +34,46 @@ routers.get('/siswa', async (req, res) => {
 routers.get('/siswa/:id', async (req, res) => {
     if (client.isConnected()) {
         const db = client.db('sekolah')
-        // kode untuk menampilkan list products res.send('menampilkan list products')
+        // kode untuk menampilkan list siswa res.send('menampilkan list siswa')
         const siswa = await db.collection('siswa').findOne({
-            _id: ObjectId(req.params.id)})
+            _id: ObjectId(req.params.id)
+        })
         res.send({
             status: 'success',
             message: 'single siswa',
             data: siswa
         })
+    } else {
+        res.send({
+            status: 'error',
+            message: 'koneksi database gagal'
+        })
+    }
+})
+
+//tambah data siswa
+routers.post('/siswa',multer().none(), async (req, res) => {
+    if (client.isConnected) {
+        const { nama, kelas } = req.body;
+        const db = client.db("sekolah");
+        const result = await db.collection("siswa").insertOne({
+            nama: nama,
+            kelas: kelas
+        })
+
+        if (result.insertedCount == 1) {
+            res.send({
+                status: 'success',
+                message: 'tambah data berhasil'
+            })
+
+        } else {
+            res.send({
+                status: 'warning',
+                message: 'tambah data gagal'
+            })
+        }
+
     } else {
         res.send({
             status: 'error',
